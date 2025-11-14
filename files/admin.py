691fbd594cb6c8010a5cdf0d3680c72826f9f20c
charles-kamga <charleskamgapr@gmail.com>
2025-11-14 09:16:25 +0100

    Revert "premiere phase de mon travail sur mon gestionnaire de documents"
    This reverts commit 508cfabf84d58d4284cdaa8a50f1536d43163079.

--- files/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-
-# Register your models here.
-from django.contrib import admin
-from .models import Folder, Document
-
-admin.site.register(Folder)
-admin.site.register(Document)
